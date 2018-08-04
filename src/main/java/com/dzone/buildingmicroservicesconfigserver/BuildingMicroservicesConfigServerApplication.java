171ee53e7224d2ffc13892837854fe775f067f24
Diego Rani Mazine <diego.mazine@livelo.com.br>
2018-08-04 14:49:42 -0300

    Initial version

---
+++ src/main/java/com/dzone/buildingmicroservicesconfigserver/BuildingMicroservicesConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.dzone.buildingmicroservicesconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingMicroservicesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingMicroservicesConfigServerApplication.class, args);
+	}
+}
